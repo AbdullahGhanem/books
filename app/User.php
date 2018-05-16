db35e4795afb2882c375dca715c7c43792c0db9d
Abdullah Ghanem <3bdullah.ghanem@gmail.com>
2018-05-16 22:52:30 +0200

    make to custom url after login

--- app/User.php
+++ app/User.php
@@ -18,6 +18,8 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    protected $redirectTo = '/';
+
     /**
      * The attributes that should be hidden for arrays.
      *
